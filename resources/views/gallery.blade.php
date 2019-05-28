d1ee7106f3cddb96ff98a9dcfd3314f80492fdfd
mhuda1 <mushaffahuda01@gmail.com>
2019-05-28 14:26:28 +0700

    adding gallery page base

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/master')
+
+@section('title', 'Homepage')
+
+@section('content')
+
+<div class='container text-center mt-3 bg-dark'>
+    <div class='row'>
+        <div class='col-lg-3'>
+            <h1>Gallery<h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum illum omnis, quos tenetur rerum praesentium velit eum provident officiis facilis.</p>
+        </div>
+    </div>
+</div>
+
+@endsection
